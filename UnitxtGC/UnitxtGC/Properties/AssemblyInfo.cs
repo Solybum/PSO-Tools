f4286e9c869b04c5ab379288f8d5cddd2b973c5c
Soleil Rojas <soleilrojas@outlook.com>
2017-04-02 20:30:17 -0600

    Added description

--- UnitxtGC/UnitxtGC/Properties/AssemblyInfo.cs
+++ UnitxtGC/UnitxtGC/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("UnitxtGC")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Program to convert gamecube unitxt files between their binary form and Json")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("UnitxtGC")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © Soleil Rojas 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
